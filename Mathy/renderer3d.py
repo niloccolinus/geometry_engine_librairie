a5bb665e324f29d3bff8e73999b3fbc9bd3cc582
Leïla P. <leila.karukera@gmail.com>
2025-06-15 15:13:09 +0200

    ✨ Update Renderer3D

--- Mathy/renderer3d.py
+++ Mathy/renderer3d.py
@@ -1,16 +1,15 @@
+"""Defines a 3D renderer class."""
 
-from Mathy import Vector4, Matrix4x4
+from Mathy import GameObject
 
 
-class Renderer3D:
-    def __init__(self, vertices: list[Vector4], indices: list[int]):
-        self.vertices = vertices  # Local-space geometry (before transform)
-        self.indices = indices  # Indices of the vertices
+class Renderer3D(object):
+    """A class to contain render data."""
 
-    def draw(self, gameobject: 'GameObject'):
-        transform_matrix = gameobject.transform.get_matrix()
-
-        print(f"Rendering object: {gameobject.name}")
-        for i, vertex in enumerate(self.vertices):
-            world_vertex = vertex.multiply_by_matrix(transform_matrix)
-            print(f"Vertex {i}: {world_vertex}")
+    def convert_local_to_world(self, game_object: GameObject):
+        """Apply world coordinates to game object."""
+        model_matrix = game_object.transform.transform_matrix
+        world_vertices = []
+        for vertex in game_object.homogeneous_vertices:
+            world_vertices.append(vertex.multiply_by_matrix(model_matrix))
+        return world_vertices

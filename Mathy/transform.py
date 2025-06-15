daaf2157d8f6cac8f500a27ec6166d1d44cfdd65
Leïla P. <leila.karukera@gmail.com>
2025-06-15 14:06:34 +0200

    ✨ Add translate, rotate and scaling methods to transform.py

--- Mathy/transform.py
+++ Mathy/transform.py
@@ -1,24 +1,41 @@
 """Defines a Transform class containing a GameObject's geometry data."""
 
-from Mathy import HomogeneousVector4, Matrix4x4
+
+from Mathy import (TranslationMatrix4x4,
+                   RotationMatrix4x4_x,
+                   RotationMatrix4x4_y,
+                   RotationMatrix4x4_z,
+                   TotalRotationMatrix4x4,
+                   HomothetyMatrix4x4,
+                   AnisotropicMatrix4x4)
 
 
 class Transform:
     """A class to represent a game object's position, rotation and scale."""
 
-    def __init__(self, position=HomogeneousVector4(0, 0, 0),
-                 rotation=0, scale=1):
+    def __init__(self):
         """Initialize a transform."""
-        self.position = position
-        self.rotation = rotation
-        self.scale = scale
+        self.transform_matrix = HomothetyMatrix4x4(1)
 
-    def get_matrix(self) -> 'Matrix4x4':
-        from Mathy import (TranslationMatrix4x4,
-                           RotationMatrix4x4,
-                           HomothetyMatrix4x4)
-        a, b, c = self.position.x, self.position.y, self.position.z
-        T = TranslationMatrix4x4(a, b, c)
-        R = RotationMatrix4x4(self.rotation)
-        S = HomothetyMatrix4x4(self.scale)
-        return T.prod(R).prod(S)
+    def translate(self, a, b, c):
+        """Apply translation."""
+        translation = TranslationMatrix4x4(a, b, c)
+        self.transform_matrix = self.transform_matrix.prod(translation)
+
+    def rotate(self, angle_x, angle_y, angle_z):
+        """Apply rotation."""
+        rot_x = RotationMatrix4x4_x(angle_x)
+        rot_y = RotationMatrix4x4_y(angle_y)
+        rot_z = RotationMatrix4x4_z(angle_z)
+        rotation = TotalRotationMatrix4x4(rot_x, rot_y, rot_z)
+        self.transform_matrix = self.transform_matrix.prod(rotation)
+
+    def homothetic_scale(self, k):
+        """Apply homothety."""
+        homothety = HomothetyMatrix4x4(k)
+        self.transform_matrix = self.transform_matrix.prod(homothety)
+
+    def anisotropic_scale(self, sx, sy, sz):
+        """Apply anisotropic scaling."""
+        anisotropic = AnisotropicMatrix4x4(sx, sy, sz)
+        self.transform_matrix = self.transform_matrix.prod(anisotropic)

c98f4d2c8495d44f8973e5c17c016fd5b3d1c0bc
Leïla P. <leila.karukera@gmail.com>
2025-06-15 03:12:02 +0200

    🚧 💩 Update Transform

--- Mathy/transform.py
+++ Mathy/transform.py
@@ -1,13 +1,24 @@
+"""Defines a Transform class containing a GameObject's geometry data."""
+
+from Mathy import HomogeneousVector4, Matrix4x4
+
+
 class Transform:
-    def __init__(self, position=None, rotation=None, scale=None):
-        from Mathy import Vector3
-        self.position = position or Vector3(0, 0, 0)
-        self.rotation = rotation or Vector3(0, 0, 0)
-        self.scale = scale or Vector3(1, 1, 1)
+    """A class to represent a game object's position, rotation and scale."""
+
+    def __init__(self, position=HomogeneousVector4(0, 0, 0),
+                 rotation=0, scale=1):
+        """Initialize a transform."""
+        self.position = position
+        self.rotation = rotation
+        self.scale = scale
 
     def get_matrix(self) -> 'Matrix4x4':
-        from Mathy import Matrix4x4, TranslationMatrix4x4, RotationMatrix4x4, HomothetyMatrix4x4
-        T = TranslationMatrix4x4(self.position)
+        from Mathy import (TranslationMatrix4x4,
+                           RotationMatrix4x4,
+                           HomothetyMatrix4x4)
+        a, b, c = self.position.x, self.position.y, self.position.z
+        T = TranslationMatrix4x4(a, b, c)
         R = RotationMatrix4x4(self.rotation)
         S = HomothetyMatrix4x4(self.scale)
         return T.prod(R).prod(S)

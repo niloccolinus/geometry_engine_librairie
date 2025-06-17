610a173d7beb5024a05771e61a95aadc243b167c
Leïla P. <leila.karukera@gmail.com>
2025-06-17 13:53:30 +0200

    ✨ Update factorial function in math_utils.py

--- Mathy/math_utils.py
+++ Mathy/math_utils.py
@@ -3,10 +3,12 @@
 pi = 3.1415926535897932384626433832795028841971  # Value of constant pi
 
 
-def factorial(n):
+def factorial(n: int):
     """Return the factorial of n."""
     if n == 1 or n == 0:
         return 1
+    elif n < 0:
+        raise ValueError("Cannot compute factorial for negative integers.")
     else:
         return n * factorial(n - 1)
 

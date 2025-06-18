c6ee8e74d0bde97ba3717d85c7385f1374c25f65
niloccolinus <niloccolin@protonmail.com>
2025-06-18 14:06:34 +0200

    ✨ Add norm, conjugate and inverse functions

--- Mathy/quaternion.py
+++ Mathy/quaternion.py
@@ -12,3 +12,17 @@ class Quaternion:
         self.x = x 
         self.y = y 
         self.z = z
+
+    @property
+    def norm(self) -> float:
+        """Calculate the norm of the quaternion (accessible as a property)."""
+        return (self.w**2 + self.x**2 + self.y**2 + self.z**2)**0.5
+
+    @property
+    def conjugate(self) -> 'Quaternion':
+        """Calculate the conjugate of the quaternion."""
+        return Quaternion(self.w, -self.x, -self.y, -self.z)
+
+    def inverse(self) -> 'Quaternion':
+        """Calculate the inverse of the quaternion."""
+        return self.conjugate / self.norm**2

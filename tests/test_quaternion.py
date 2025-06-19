89f00b13a3bfbd520d82f7e906aa2728ccf4faaf
niloccolinus <niloccolin@protonmail.com>
2025-06-19 10:02:16 +0200

    ✨ ✅  Create and pass tests for quaternion class
    - unit tests for : normalize, conjugate, inverse, norm & constructor

---
+++ tests/test_quaternion.py
@@ -0,0 +1,54 @@
+"""Tests for verifying proper Quaternion functionality."""
+
+from Mathy import Quaternion
+import pytest
+
+
+def test_constructor():
+    """Test arguments' types for Quaternion constructor."""
+    # Ints
+    Quaternion(1, 2, 3, 4)
+
+    # Floats
+    Quaternion(1.0, 2.0, 3.0, 4.0)
+
+    # Floats and ints
+    Quaternion(1, 2.0, 3, 4.0)
+
+    # Invalid types
+    with pytest.raises(TypeError, match=r".* must be floats or ints.*"):
+        Quaternion(0, "a", None, [])
+
+
+def test_norm():
+    """Test norm() method."""
+    # Norm zero
+    assert abs(Quaternion(0, 0, 0, 0).norm) < 1e-9
+
+    # Norm positive coordinates
+    assert abs(Quaternion(2, 4, 4, 0).norm - 6.0) < 1e-9
+
+    # Norm negative coordinates
+    assert abs(Quaternion(-2, -4, 4, 0).norm - 6.0) < 1e-9
+
+
+def test_normalize():
+    """Test normalize() method."""
+    # Normalize quaternion
+    assert Quaternion(2, 4, 4, 0).normalize() == Quaternion(1/3, 2/3, 2/3, 0)
+
+    # Norm == 0
+    with pytest.raises(ValueError, match=r"Cannot normalize a zero quaternion.*"):  # noqa: E501
+        Quaternion(0, 0, 0, 0).normalize()
+
+
+def test_conjugate():
+    """Test conjugate() method."""
+    assert Quaternion(1, 2, 3, 4).conjugate == Quaternion(1, -2, -3, -4)
+
+
+def test_inverse():
+    """Test inverse() method."""
+    q = Quaternion(1, 2, 3, 4)
+    assert q.inverse == q.conjugate / q.norm**2
+

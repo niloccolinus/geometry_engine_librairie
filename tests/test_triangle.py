2995b9a5f1f25becced39a8aaf696cea8d0189b5
Leïla P. <leila.karukera@gmail.com>
2025-06-23 04:11:34 +0200

    ✅ Update float comparisons in test_triangle

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -14,30 +14,33 @@ triangle3 = Triangle([-2.5, 0], [0, 5.5], [2.5, 0])
 def test_side_lengths():
     """Test side_lengths() method."""
     # Lengths triangle 1
-    assert triangle1.side_lengths() == (
-        1.0, math_utils.root(2), 1.0)
+    a, b, c = triangle1.side_lengths()
+    a1, b1, c1 = (1.0, math_utils.root(2), 1.0)
+    assert math_utils.is_close(a, a1) and math_utils.is_close(b, b1) and math_utils.is_close(c, c1)  # noqa: E501
     # Lengths triangle 2
-    assert triangle2.side_lengths() == (
-        math_utils.root(2), math_utils.root(2), 2.0)
+    d, e, f = triangle2.side_lengths()
+    d1, e1, f1 = (math_utils.root(2), math_utils.root(2), 2.0)
+    assert math_utils.is_close(d, d1) and math_utils.is_close(e, e1) and math_utils.is_close(f, f1)  # noqa: E501
     # Lengths triangle 3
-    assert triangle3.side_lengths() == (
-        math_utils.root(36.5), math_utils.root(36.5), 5.0)
+    g, h, i = triangle3.side_lengths()
+    g1, h1, i1 = (math_utils.root(36.5), math_utils.root(36.5), 5.0)
+    assert math_utils.is_close(g, g1) and math_utils.is_close(h, h1) and math_utils.is_close(i, i1)  # noqa: E501
 
 
 def test_perimeter():
     """Test perimeter() method."""
     # Perimeter triangle 1
-    assert triangle1.perimeter() == float(2 + math_utils.root(2))
+    assert abs(triangle1.perimeter() - float(2 + math_utils.root(2))) < 1e-9
     # Perimeter triangle 2
-    assert triangle2.perimeter() == float(2 + math_utils.root(2) * 2)
+    assert abs(triangle2.perimeter() - float(2 + math_utils.root(2) * 2)) < 1e-9  # noqa: E501
     # Perimeter triangle 3
-    assert triangle3.perimeter() == float(5 + math_utils.root(36.5) * 2)
+    assert abs(triangle3.perimeter() - float(5 + math_utils.root(36.5) * 2)) < 1e-9  # noqa: E501
 
 
 def test_area():
     """Test area() method."""
     # Area triangle 1
-    assert triangle1.area() == 0.5
+    assert abs(triangle1.area() - 0.5) < 1e-9
     # Area triangle 2
     # Use is_close to allow for small floating-point errors
     assert math_utils.is_close(triangle2.area(), 1.0)
@@ -59,11 +62,25 @@ def test_right_angled():
 def test_get_vertices():
     """Test get_vertices() method."""
     # Vertices triangle 1
-    assert triangle1.get_vertices() == ([0, 0], [0, 1], [1, 0])
+    vertices1 = triangle1.get_vertices()
+    coords1 = ([0, 0], [0, 1], [1, 0])
+    for vertex, coord in zip(vertices1, coords1):
+        for i in range(0, 2):
+            assert math_utils.is_close(vertex[i], coord[i])
+
     # Vertices triangle 2
-    assert triangle2.get_vertices() == ([-1, 0], [0, 1], [1, 0])
+    vertices2 = triangle2.get_vertices()
+    coords2 = ([-1, 0], [0, 1], [1, 0])
+    for vertex, coord in zip(vertices2, coords2):
+        for i in range(0, 2):
+            assert math_utils.is_close(vertex[i], coord[i])
+
     # Vertices triangle 3
-    assert triangle3.get_vertices() == ([-2.5, 0], [0, 5.5], [2.5, 0])
+    vertices3 = triangle3.get_vertices()
+    coords3 = ([-2.5, 0], [0, 5.5], [2.5, 0])
+    for vertex, coord in zip(vertices3, coords3):
+        for i in range(0, 2):
+            assert math_utils.is_close(vertex[i], coord[i])
 
 
 # Triangle 3D Tests Unitaires
@@ -78,27 +95,30 @@ triangle6 = Triangle3D([0, -2.5, 0], [0, 0, 5.5], [2.5, 0, 0])
 def test_side_lengths_3d():
     """Test side_lengths() method."""
     # Lengths triangle 4
-    # assert triangle4.side_lengths() == (
-    #     math_utils.root(2), math_utils.root(2), math_utils.root(2))
+    a, b, c = triangle4.side_lengths()
+    a1, b1, c1 = (math_utils.root(2), math_utils.root(2), math_utils.root(2))
+    assert math_utils.is_close(a, a1) and math_utils.is_close(b, b1) and math_utils.is_close(c, c1)  # noqa: E501
     # Lengths triangle 5
-    assert triangle5.side_lengths() == (
-        math_utils.root(2), math_utils.root(2), 2.0)
+    d, e, f = triangle5.side_lengths()
+    d1, e1, f1 = (math_utils.root(2), math_utils.root(2), 2.0)
+    assert math_utils.is_close(d, d1) and math_utils.is_close(e, e1) and math_utils.is_close(f, f1)  # noqa: E501
     # Lengths triangle 6
-    assert triangle6.side_lengths() == (
-        math_utils.root(36.5), math_utils.root(36.5), math_utils.root(12.5))
+    g, h, i = triangle6.side_lengths()
+    g1, h1, i1 = (math_utils.root(36.5), math_utils.root(36.5), math_utils.root(12.5))  # noqa: E501
+    assert math_utils.is_close(g, g1) and math_utils.is_close(h, h1) and math_utils.is_close(i, i1)  # noqa: E501
 
 
 def test_perimeter_3d():
     """Test perimeter() method."""
     # Perimeter triangle 4
-    assert triangle4.perimeter() == float(
-        math_utils.root(2) * 3)
+    assert abs(triangle4.perimeter() - float(
+        math_utils.root(2) * 3)) < 1e-9
     # Perimeter triangle 5
-    assert triangle5.perimeter() == float(
-        2 + math_utils.root(2) * 2)
+    assert abs(triangle5.perimeter() - float(
+        2 + math_utils.root(2) * 2)) < 1e-9
     # Perimeter triangle 6
-    assert triangle6.perimeter() == float(
-        math_utils.root(12.5) + math_utils.root(36.5) * 2)
+    assert abs(triangle6.perimeter() - float(
+        math_utils.root(12.5) + math_utils.root(36.5) * 2)) < 1e-9
 
 
 def test_area_3d():
@@ -126,8 +146,22 @@ def test_right_angled_3d():
 def test_get_vertices_3d():
     """Test get_vertices() method."""
     # Vertices triangle 4
-    assert triangle4.get_vertices() == ([0, 0, 1], [0, 1, 0], [1, 0, 0])
+    vertices4 = triangle4.get_vertices()
+    coords4 = ([0, 0, 1], [0, 1, 0], [1, 0, 0])
+    for vertex, coord in zip(vertices4, coords4):
+        for i in range(0, 3):
+            assert math_utils.is_close(vertex[i], coord[i])
+
     # Vertices triangle 5
-    assert triangle5.get_vertices() == ([-1, 0, 0], [0, 0, 1], [1, 0, 0])
+    vertices5 = triangle5.get_vertices()
+    coords5 = ([-1, 0, 0], [0, 0, 1], [1, 0, 0])
+    for vertex, coord in zip(vertices5, coords5):
+        for i in range(0, 3):
+            assert math_utils.is_close(vertex[i], coord[i])
+
     # Vertices triangle 6
-    assert triangle6.get_vertices() == ([0, -2.5, 0], [0, 0, 5.5], [2.5, 0, 0])
+    vertices6 = triangle6.get_vertices()
+    coords6 = ([0, -2.5, 0], [0, 0, 5.5], [2.5, 0, 0])
+    for vertex, coord in zip(vertices6, coords6):
+        for i in range(0, 3):
+            assert math_utils.is_close(vertex[i], coord[i])
